f1bb8a4af513b7fd009cd3dcdabd8d10f15f93e7
Reas Vyn <reasnov.app@gmail.com>
2026-03-02 07:45:19 +0700

    test(school): update requirement reference from [ARC01-INST-01] to [SYRS-F-101]

--- modules/School/tests/Unit/Services/SchoolServiceTest.php
+++ modules/School/tests/Unit/Services/SchoolServiceTest.php
@@ -33,10 +33,10 @@ describe('School Service', function () {
         $service->create(['name' => 'Unauthorized School']);
     })->throws(\Illuminate\Auth\Access\AuthorizationException::class);
 
-    test('it validates Institutional Code format (must be 8 digits) [ARC01-INST-01]', function () {
+    test('it validates Institutional Code format (must be at least 3 characters) [SYRS-F-101]', function () {
         Gate::shouldReceive('authorize')->andReturn(true);
         $service = app(SchoolService::class);
 
-        $service->create(['name' => 'Test School', 'institutional_code' => '123']);
+        $service->create(['name' => 'Test School', 'institutional_code' => '12']);
     })->throws(AppException::class);
 });
